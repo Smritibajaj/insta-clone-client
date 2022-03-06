3a2d1d8fe6fe1af02525c1ae5f90f85cfa81f6ee
simmy bajaj <simmybajaj1603@gmail.com>
2022-03-06 11:57:30 +0530

    changes- initial

--- src/Instagram.js
+++ src/Instagram.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Route, Outlet, Routes } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const PostComponent = (props) => {
